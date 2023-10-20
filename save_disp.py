653e7447b91f3cd4dd930aac29586984fc2856c0
yuankang <94879837+yuankang1234@users.noreply.github.com>
2023-10-20 21:35:54 +0800

    Add files via upload

---
+++ save_disp.py
@@ -0,0 +1,86 @@
+from __future__ import print_function, division
+import argparse
+import os
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim as optim
+import torch.utils.data
+from torch.autograd import Variable
+import torchvision.utils as vutils
+import torch.nn.functional as F
+import numpy as np
+import time
+from tensorboardX import SummaryWriter
+from datasets import __datasets__
+from models import __models__
+from utils import *
+from torch.utils.data import DataLoader
+import gc
+import skimage.io
+
+cudnn.benchmark = True
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+
+
+parser = argparse.ArgumentParser(description='PCW-Net: Pyramid Combination and Warping Cost Volume for Stereo Matching')
+parser.add_argument('--model', default='gwcnet-gc', help='select a model structure', choices=__models__.keys())
+parser.add_argument('--maxdisp', type=int, default=192, help='maximum disparity')
+
+parser.add_argument('--dataset', default='kitti', help='dataset name', choices=__datasets__.keys())
+parser.add_argument('--datapath', default="/mnt/9c69d5cd-01cb-4603-b7c6-06d924734d0c/CYJ/dataset/kitti2015/", help='data path')
+
+parser.add_argument('--testlist', default="./filenames_1/kitti15_test.txt", help='testing list')
+parser.add_argument('--loadckpt', default="./finetune_15/checkpoint_001966.ckpt", help='load the weights from a specific checkpoint')
+
+# parse arguments
+args = parser.parse_args()
+
+# dataset, dataloader
+StereoDataset = __datasets__[args.dataset]
+test_dataset = StereoDataset(args.datapath, args.testlist, False)
+TestImgLoader = DataLoader(test_dataset, 1, shuffle=False, num_workers=0, drop_last=False)
+
+# model, optimizer
+model = __models__[args.model](args.maxdisp)
+model = nn.DataParallel(model)
+model.cuda()
+
+# load parameters
+print("loading model {}".format(args.loadckpt))
+state_dict = torch.load(args.loadckpt)
+model.load_state_dict(state_dict['model'])
+
+
+def test():
+    os.makedirs('./pre_train_230918', exist_ok=True)
+    for batch_idx, sample in enumerate(TestImgLoader):
+        start_time = time.time()
+        disp_est_np = tensor2numpy(test_sample(sample))
+        top_pad_np = tensor2numpy(sample["top_pad"])
+        right_pad_np = tensor2numpy(sample["right_pad"])
+        left_filenames = sample["left_filename"]
+        print('Iter {}/{}, time = {:3f}'.format(batch_idx, len(TestImgLoader),
+                                                time.time() - start_time))
+
+        for disp_est, top_pad, right_pad, fn in zip(disp_est_np, top_pad_np, right_pad_np, left_filenames):
+            assert len(disp_est.shape) == 2
+            disp_est = np.array(disp_est[top_pad:, :-right_pad], dtype=np.float32)
+            #fn = os.path.join("predictions", fn.split('/')[-1])
+            fn = os.path.join("./pre_train_230918/", fn.split('/')[-1])
+            print("saving to", fn, disp_est.shape)
+            disp_est_uint = np.round(disp_est * 256).astype(np.uint16)
+            skimage.io.imsave(fn, disp_est_uint)
+
+
+# test one sample
+@make_nograd_func
+def test_sample(sample):
+    model.eval()
+    disp_ests, pred3 = model(sample['left'].cuda(), sample['right'].cuda())
+    return disp_ests[-1]
+
+
+if __name__ == '__main__':
+    test()
